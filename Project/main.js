43023929d95cb5d42fab506bc7fddb1f30b05f8e
SenTW <esensultanov23@gmail.com>
2025-01-10 06:53:16 +0800

    Add files via upload

---
+++ Project/main.js
@@ -0,0 +1,80 @@
+const scrollLine = document.querySelector('.scroll-line');
+
+    function updateScrollProgress() {
+      const windowHeight = window.innerHeight;
+      const fullHeight = document.body.clientHeight;
+      const scrolled = window.scrollY;
+      const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
+      scrollLine.style.width = `${percentScrolled}%`;
+    }
+
+    updateScrollProgress();
+
+    let ticking = false;
+    window.addEventListener('scroll', () => {
+      if (!ticking) {
+        window.requestAnimationFrame(() => {
+          updateScrollProgress();
+          ticking = false;
+        });
+        ticking = true;
+      }
+    });
+
+    window.addEventListener('resize', updateScrollProgress);
+
+    AOS.init({
+        duration : 2000,
+      })
+      
+
+      function scrollToSection(sectionId) {
+        const section = document.getElementById(sectionId);
+        section.scrollIntoView({
+           behavior: 'smooth'
+        }); // Smooth scroll to the section
+     }
+     
+
+
+const gra = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
+const init = function(){
+  let items = document.querySelectorAll('section');
+  for (let i = 0; i < items.length; i++){
+    items[i].style.background = randomColor({luminosity: 'light'});
+  }
+  cssScrollSnapPolyfill()
+}
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('image-animation');
+      }
+    });
+  });
+  
+  const viewbox = document.querySelectorAll('.image');
+  viewbox.forEach(image => {
+    observer.observe(image);
+  });
+
+document.addEventListener("scroll", () => {
+    const hero = document.querySelector(".hero");
+    const scrollPosition = window.scrollY;
+  
+    const fadePoint = window.innerHeight / 2;
+  
+    if (scrollPosition > fadePoint) {
+      hero.classList.add("fade-out");
+    } else {
+      hero.classList.remove("fade-out");
+    }
+  });
+
+// init();
